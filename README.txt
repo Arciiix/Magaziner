0d2ea48b471b8405ecd0bb31fe726b4d332abf49
Arciiix <artur.nowak100@gmail.com>
2019-12-02 20:54:42 +0100

    Version ~0.4

--- README.txt
+++ README.txt
@@ -1,1 +1,24 @@
 This is a simple magaziner, like you can see you storage, you can easily add new products. It's just for fun, so it's not good! :)
+
+
+
+Path/version logs:
+
+
+
+
+
+
+version ~0.4:
+Added basic design for homepage. Now it shows a table and a add button.
+
+
+
+
+version 0.2
+Added basic html files and css stylesheet.
+
+
+
+version 0.1
+Added readme file.
--- index.php
+++ index.php
@@ -9,9 +9,35 @@
     <title>Magazynier</title>
 
     <link rel="stylesheet" type="text/css" href="css/index.css">
-    
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 <body>
+
+<h1 id = 'mainHeader'>Magazynier
+
+<label title="Dodaj nową rzecz" onClick = "window.location.href = 'add.php'"><i alt="Dodaj" id = "addIcon" style="margin-right: 20px; float: right; font-size: 60px"class="fa fa-plus-square" aria-hidden="true"></i> </label>
+</h1>
+
+<table class="table-fill">
+<thead>
+<tr>
+<th class="text-center">Nazwa</th>
+<th class="text-center">Kategoria</th>
+<th class="text-center">Data ważności</th>
+</tr>
+</thead>
+<tbody class="table-hover">
+<tr>
+<td class="text-center">Tutaj ma byc nazwa</td>
+<td class="text-center">Tutaj ma byc Kategoria</td>
+<td class="text-center">Tutaj ma byc data waznosci</td>
+</tr>
+
+
+</tbody>
+</table>
     
 </body>
 </html>
